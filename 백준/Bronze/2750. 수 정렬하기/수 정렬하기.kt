65906fec84722a9cfbfa8814043194f7b5782657
Solunax <97011241+Solunax@users.noreply.github.com>
2022-04-24 21:03:30 +0900

    [Bronze I] Title: 수 정렬하기, Time: 288 ms, Memory: 23160 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.kt
@@ -0,0 +1,17 @@
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.min
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val count = sc.nextInt()
+    val array = Array<Int>(count){0}
+    sc.nextLine()
+
+    for(i in 0 until count){
+        array[i] = sc.nextInt()
+    }
+
+    array.sorted().forEach{ println(it) }
+}
