bbd4976398ee79815c42c3cafc7f4e132a53a44a
Solunax <97011241+Solunax@users.noreply.github.com>
2022-04-27 22:17:21 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1052 ms, Memory: 85616 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.kt
@@ -0,0 +1,30 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import kotlin.collections.ArrayList
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = System.out.bufferedWriter()
+
+    val inputNumber = br.readLine().toInt()
+    val arrayXY = ArrayList<Pair<Int, Int>>()
+
+    for(i in 0 until inputNumber){
+        val input = br.readLine().split(" ").map{ it.toInt() }
+        arrayXY.add(Pair(input[0], input[1]))
+    }
+
+    arrayXY.sortWith(Comparator{ a, b ->
+        if(a.second == b.second)
+            a.first - b.first
+        else
+            a.second - b.second
+    })
+
+    for(i in 0 until inputNumber)
+        bw.write("${arrayXY[i].first} ${arrayXY[i].second}\n")
+
+    bw.flush()
+    bw.close()
+    br.close()
+}
