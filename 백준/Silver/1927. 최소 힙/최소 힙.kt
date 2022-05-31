f6bd5fe441b32973e95d61c747f7849c98a50bf8
Solunax <97011241+Solunax@users.noreply.github.com>
2022-05-31 09:39:02 +0900

    [Silver II] Title: 최소 힙, Time: 296 ms, Memory: 26952 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.kt
@@ -0,0 +1,29 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.LinkedList
+import java.util.PriorityQueue
+import kotlin.math.max
+import kotlin.math.min
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    val n = br.readLine().toInt()
+    val queue = PriorityQueue<Int>()
+
+    for(i in 0 until n){
+        val input = br.readLine().toInt()
+        if(input == 0){
+            if(queue.size == 0 )
+                bw.write("0\n")
+            else
+                bw.write("${queue.poll()}\n")
+        }
+        else
+            queue.add(input)
+    }
+
+    bw.close()
+    br.close()
+}
