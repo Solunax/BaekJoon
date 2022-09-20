5747b6a28a41d5844385e576a060403e500d75e9
Solunax <97011241+Solunax@users.noreply.github.com>
2022-09-20 20:18:54 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 96 ms, Memory: 12288 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.kt
@@ -0,0 +1,13 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    val n = br.readLine().toLong()
+
+    bw.write("${n*4}")
+    bw.close()
+    br.close()
+}
