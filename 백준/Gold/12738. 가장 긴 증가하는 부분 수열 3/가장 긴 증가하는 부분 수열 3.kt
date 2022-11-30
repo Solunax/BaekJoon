f5d8277ba2a621bb2a370fe20f4d3303fcd0042b
Solunax <97011241+Solunax@users.noreply.github.com>
2022-12-01 00:59:56 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 752 ms, Memory: 223416 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.kt
@@ -0,0 +1,39 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    val n = br.readLine().toInt()
+    val array = br.readLine().split(" ").map { it.toInt() }
+    val lis = Array(n){0}
+
+    lis[0] = array[0]
+    var size = 1
+
+    for(i in 1 until n){
+        val value = array[i]
+        if(value > lis[size - 1]){
+            size++
+            lis[size - 1] = value
+        }
+        else{
+            var low = 0
+            var high = size
+            while(low < high){
+                val center = (high + low) / 2
+                if(lis[center] < value)
+                    low = center + 1
+                else
+                    high = center
+            }
+            lis[low] = value
+        }
+    }
+
+    bw.write("$size")
+
+    br.close()
+    bw.close()
+}
