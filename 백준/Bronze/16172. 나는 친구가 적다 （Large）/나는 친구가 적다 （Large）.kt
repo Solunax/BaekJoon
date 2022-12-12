221da6290143b1378a2a41b796ffbf3d83397601
Solunax <97011241+Solunax@users.noreply.github.com>
2022-12-12 17:51:17 +0900

    [Bronze II] Title: 나는 친구가 적다 (Large), Time: 288 ms, Memory: 21044 KB -BaekjoonHub

---
+++ 백준/Bronze/16172. 나는 친구가 적다 （Large）/나는 친구가 적다 （Large）.kt
@@ -0,0 +1,59 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    val origin = StringBuilder()
+
+    br.readLine().forEach {
+        if(it !in '0'..'9')
+            origin.append(it)
+    }
+    val str = origin.toString()
+    val find = br.readLine()
+
+    bw.write("${kmp(str, find)}")
+    br.close()
+    bw.close()
+}
+
+fun kmp(origin : String, pattern : String) : Int{
+    val arr = makeTable(pattern)
+
+    var index = 0
+    for(i in origin.indices){
+        while (index > 0 && origin[i] != pattern[index]){
+            index = arr[index-1]
+        }
+
+        if(origin[i] == pattern[index]){
+            if(index == pattern.length-1){
+                return 1
+            }else
+                index += 1
+        }
+    }
+
+    return 0
+}
+
+fun makeTable(pattern : String) : Array<Int>{
+    val n = pattern.length
+    val arr = Array(n){0}
+
+    var index = 0
+    for(i in 1 until n){
+        while(index > 0 && pattern[i] != pattern[index]){
+            index = arr[index-1]
+        }
+
+        if(pattern[i] == pattern[index]){
+            index += 1
+            arr[i] = index
+        }
+    }
+
+    return arr
+}
