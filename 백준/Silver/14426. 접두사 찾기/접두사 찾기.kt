2b9143029d2832f8a634bf0e120a20c8cd390974
Solunax <97011241+Solunax@users.noreply.github.com>
2023-02-18 00:41:31 +0900

    [Silver I] Title: 접두사 찾기, Time: 664 ms, Memory: 60312 KB -BaekjoonHub

--- 백준/Silver/14426. 접두사 찾기/접두사 찾기.kt
+++ 백준/Silver/14426. 접두사 찾기/접두사 찾기.kt
@@ -6,25 +6,62 @@ val bw = System.out.bufferedWriter()
 
 fun main() {
     val (n, m) = br.readLine().split(" ").map{ it.toInt() }
-    val words = Array(n){""}
+    val trie = Trie()
 
     repeat(n){
-        words[it] = br.readLine()
+        trie.insert(br.readLine())
     }
 
     var result = 0
     repeat(m){
-        val input = br.readLine()
-        for(i in 0 until n){
-            if(words[i].substring(0, input.length) == input){
-                result++
-                break
-            }
-        }
+        if(trie.containCheck(br.readLine()))
+            result++
     }
 
     bw.write("$result")
 
     br.close()
     bw.close()
+}
+
+class Trie{
+    private var root = TrieNode()
+
+    fun insert(str : String){
+        var thisNode = root
+
+        for(char in str)
+            thisNode = thisNode.getChild().computeIfAbsent(char) { TrieNode() }
+
+        root.setLast(true)
+    }
+
+    fun containCheck(str : String): Boolean {
+        var thisNode = root
+
+        for(char in str){
+            val node = thisNode.getChild()[char] ?: return false
+
+            thisNode = node
+        }
+
+        return true
+    }
+}
+
+class TrieNode{
+    private val child = HashMap<Char, TrieNode>()
+    private var isLast : Boolean = false
+
+    fun getChild() : HashMap<Char, TrieNode> {
+        return this.child
+    }
+
+    fun isLastChar() : Boolean{
+        return this.isLast
+    }
+
+    fun setLast(value : Boolean){
+        this.isLast = value
+    }
 }
