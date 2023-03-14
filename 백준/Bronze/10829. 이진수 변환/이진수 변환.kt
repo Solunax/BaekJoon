799baaca933b44494d55952a6cf525a570380a2e
Solunax <97011241+Solunax@users.noreply.github.com>
2023-03-15 00:22:47 +0900

    [Bronze II] Title: 이진수 변환, Time: 92 ms, Memory: 12508 KB -BaekjoonHub

---
+++ 백준/Bronze/10829. 이진수 변환/이진수 변환.kt
@@ -0,0 +1,33 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.math.BigInteger
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    val input = br.readLine().toBigInteger()
+    bw.write(convert(input))
+
+    br.close()
+    bw.close()
+}
+
+fun convert(v1 : BigInteger) : String{
+    var n = v1
+    val sb = StringBuilder()
+    val zero = BigInteger("0")
+    val two = BigInteger("2")
+
+    while(n > zero){
+        val temp = n.remainder(two)
+        if(temp == zero)
+            sb.append("0")
+        else
+            sb.append("1")
+
+        n = n.divide(two)
+    }
+
+    return sb.reverse().toString()
+}
