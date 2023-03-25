80014d38a284bf2335ff45e16077c6651fd06a81
Solunax <97011241+Solunax@users.noreply.github.com>
2023-03-26 00:49:09 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 92 ms, Memory: 12248 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.kt
@@ -0,0 +1,14 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import kotlin.math.abs
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    val n = br.readLine().toInt()
+    bw.write("$n\n1")
+
+    br.close()
+    bw.close()
+}
