6e8dc65b1380142f7d8fa33ac375eaa9af76e025
Solunax <97011241+Solunax@users.noreply.github.com>
2023-04-08 02:49:30 +0900

    [Bronze III] Title: 더하기, Time: 128 ms, Memory: 20760 KB -BaekjoonHub

---
+++ 백준/Bronze/9085. 더하기/더하기.kt
@@ -0,0 +1,21 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    repeat(br.readLine().toInt()){
+        br.readLine()
+
+        var sum = 0
+        br.readLine().split(" ").map { it.toInt() }.forEach {
+            sum += it
+        }
+
+        bw.write("$sum\n")
+    }
+
+    br.close()
+    bw.close()
+}
