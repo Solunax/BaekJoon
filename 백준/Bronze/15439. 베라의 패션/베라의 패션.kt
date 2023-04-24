3e37258d7e41185d9ded6f3560740c8de4b22259
Solunax <97011241+Solunax@users.noreply.github.com>
2023-04-25 00:48:15 +0900

    [Bronze IV] Title: 베라의 패션, Time: 92 ms, Memory: 12228 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.kt
@@ -0,0 +1,13 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    val n = br.readLine().toInt()
+    bw.write("${(n - 1) * n}")
+
+    bw.close()
+    br.close()
+}
