8d6d4e0a4e2506ffb95031ba88cfa32c946c1f92
Solunax <97011241+Solunax@users.noreply.github.com>
2023-06-20 01:55:56 +0900

    [Bronze IV] Title: 한글, Time: 88 ms, Memory: 12300 KB -BaekjoonHub

---
+++ 백준/Bronze/11282. 한글/한글.kt
@@ -0,0 +1,12 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    bw.write("${(44031 + br.readLine().toInt()).toChar()}")
+
+    bw.close()
+    br.close()
+}
