8198e36bc81dc809775db0c7b97172ba82c7abc1
Solunax <97011241+Solunax@users.noreply.github.com>
2023-06-28 02:27:24 +0900

    [Silver III] Title: 터렛, Time: 164 ms, Memory: 21368 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.kt
+++ 백준/Silver/1002. 터렛/터렛.kt
@@ -1,54 +1,45 @@
-import java.util.*
-import kotlin.math.PI
-import kotlin.math.round
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import kotlin.math.abs
 import kotlin.math.sqrt
 
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
 fun main() {
-    val sc = Scanner(System.`in`)
+    repeat(br.readLine().toInt()){
+        val input = br.readLine().split(" ").map { it.toInt() }
+        val (x1, y1, r1) = Triple(input[0], input[1], input[2])
+        val (x2, y2, r2) = Triple(input[3], input[4], input[5])
 
-    val case = sc.nextInt()
-    sc.nextLine()
-
-    for(i in 1..case){
-        var (a, b) = Pair(0, 0)
-        val (x1, y1, r1) = Triple(sc.nextInt(), sc.nextInt(), sc.nextInt())
-        val (x2, y2, r2) = Triple(sc.nextInt(), sc.nextInt(), sc.nextInt())
-
-        if(x1 == x2 && y1 == y2 && r1 == r2){
+        if(x1 == x2 && y1 == y2 && r1 == r2)
             println(-1)
-            continue
+        else{
+            val a = abs(x1 - x2)
+            val b = abs(y1 - y2)
+            val distance = sqrt((a * a).toDouble() + (b * b).toDouble())
+
+            val big = if(r1 > r2)
+                r1
+            else
+                r2
+
+            val small = if(r1 > r2)
+                r2
+            else
+                r1
+
+            if(big - small < distance && distance < big + small)
+                println(2)
+            else if((big + small).toDouble() == distance || (big - small).toDouble() == distance)
+                println(1)
+            else if(big + small < distance || distance < big - small)
+                println(0)
+            else
+                println(-1)
         }
-        
-        a = if(x1 > x2)
-            x1 - x2
-        else
-            x2 - x1
-
-        b = if(y1 > y2)
-            y1 - y2
-        else
-            y2 - y1
-
-        val distance = sqrt((a * a).toDouble() + (b * b).toDouble())
-
-
-        val big = if(r1 > r2)
-            r1
-        else
-            r2
-
-        val small = if(r1 > r2)
-            r2
-        else
-            r1
-
-        if(big - small < distance && distance < big + small)
-            println(2)
-        else if((big + small).toDouble() == distance || (big - small).toDouble() == distance)
-            println(1)
-        else if(big + small < distance || distance < big - small)
-            println(0)
-        else
-            println(-1)
     }
+
+    bw.close()
+    br.close()
 }
