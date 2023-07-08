8174d4d8fe1ce54d17cb817b0a2b4bb951461d52
Solunax <97011241+Solunax@users.noreply.github.com>
2023-07-09 00:38:46 +0900

    [Bronze III] Title: 종이접기, Time: 772 ms, Memory: 154820 KB -BaekjoonHub

---
+++ 백준/Bronze/16504. 종이접기/종이접기.kt
@@ -0,0 +1,19 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    var sum = 0L
+
+    repeat(br.readLine().toInt()){
+        br.readLine().split(" ").map { it.toInt() }.forEach {
+            sum += it
+        }
+    }
+    bw.write("$sum")
+
+    bw.close()
+    br.close()
+}
