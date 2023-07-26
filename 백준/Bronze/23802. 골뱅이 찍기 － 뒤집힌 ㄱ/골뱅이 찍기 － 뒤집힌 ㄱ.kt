f6d3838cf462198ffe1f0a68ae95b1ec438a7f7b
Solunax <97011241+Solunax@users.noreply.github.com>
2023-07-27 02:35:33 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㄱ, Time: 116 ms, Memory: 13064 KB -BaekjoonHub

---
+++ 백준/Bronze/23802. 골뱅이 찍기 － 뒤집힌 ㄱ/골뱅이 찍기 － 뒤집힌 ㄱ.kt
@@ -0,0 +1,25 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    val n = br.readLine().toInt()
+
+    for(i in 1..n * 5){
+        if(i <= n){
+            repeat(n * 5){
+                bw.write("@")
+            }
+        }else{
+            repeat(n){
+                bw.write("@")
+            }
+        }
+        bw.write("\n")
+    }
+
+    bw.close()
+    br.close()
+}
