022796488dba2384732b3a5187b70e742cbe2f3a
Solunax <97011241+Solunax@users.noreply.github.com>
2023-08-03 04:16:47 +0900

    [Bronze IV] Title: Archivist, Time: 128 ms, Memory: 18060 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.kt
@@ -0,0 +1,16 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    val archive = listOf("ITMO", "SPbSU", "SPbSU", "ITMO", "ITMO", "SPbSU", "ITMO", "ITMO", "ITMO", "ITMO", "ITMO",
+    "PetrSU, ITMO", "SPbSU", "SPbSU", "ITMO", "ITMO", "ITMO", "ITMO", "SPbSU", "ITMO", "ITMO",
+    "ITMO", "ITMO", "SPbSU", "ITMO")
+
+    bw.write(archive[br.readLine().toInt() - 1995])
+
+    bw.close()
+    br.close()
+}
