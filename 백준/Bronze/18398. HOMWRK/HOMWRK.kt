2cafc4c33f604276ca451afae4cb7d723116130e
Solunax <97011241+Solunax@users.noreply.github.com>
2023-11-06 00:47:44 +0900

    [Bronze IV] Title: HOMWRK, Time: 152 ms, Memory: 21248 KB -BaekjoonHub

---
+++ 백준/Bronze/18398. HOMWRK/HOMWRK.kt
@@ -0,0 +1,18 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    repeat(br.readLine().toInt()){
+        repeat(br.readLine().toInt()){
+            val (a, b) = br.readLine().split(" ").map { it.toInt() }
+
+            bw.write("${a + b} ${a * b}\n")
+        }
+    }
+
+    bw.close()
+    br.close()
+}
