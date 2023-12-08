a94f6eaa9adbeb69b8ff50afef84bf040f1ea51f
Solunax <97011241+Solunax@users.noreply.github.com>
2023-12-09 00:47:26 +0900

    [Silver V] Title: 타일 장식물, Time: 96 ms, Memory: 12316 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.kt
@@ -0,0 +1,19 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    val n = br.readLine().toInt()
+    val dp = Array(n + 1){0L}
+    dp[1] = 1
+
+    for (i in 2 .. n) {
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }
+    bw.write("${(dp[n] * 2) + ((dp[n - 1] + dp[n]) * 2)}")
+
+    bw.close()
+    br.close()
+}
