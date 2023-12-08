f545af2028cdc6c1a69e963780c7296dd4a6202b
Solunax <97011241+Solunax@users.noreply.github.com>
2023-12-09 00:46:41 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 92 ms, Memory: 12324 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.kt
@@ -0,0 +1,23 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    val n = br.readLine().toInt()
+    val dp = Array(n + 1){0}
+
+    dp[1] = 1
+    if (n > 1) {
+        dp[2] = 3
+    }
+
+    for (i in 3 .. n) {
+        dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007
+    }
+    bw.write("${dp[n]}")
+
+    bw.close()
+    br.close()
+}
