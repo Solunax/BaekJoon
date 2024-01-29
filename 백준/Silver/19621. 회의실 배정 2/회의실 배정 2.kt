7aa1d5398f2d55975ed537d2f9fff8fadd53d00a
Solunax <97011241+Solunax@users.noreply.github.com>
2024-01-30 00:27:47 +0900

    [Silver II] Title: 회의실 배정 2, Time: 136 ms, Memory: 21100 KB -BaekjoonHub

---
+++ 백준/Silver/19621. 회의실 배정 2/회의실 배정 2.kt
@@ -0,0 +1,28 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import kotlin.math.max
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    val n = br.readLine().toInt()
+    val person = Array(25) { 0 }
+    val dp = Array(25) { 0 }
+
+    repeat(n) {
+        val (_, _, p) = br.readLine().split(" ").map { v -> v.toInt() }
+        person[it] = p
+    }
+
+    dp[0] = person[0]
+    dp[1] = max(person[0], person[1])
+
+    for (i in 2 until n) {
+        dp[i] = max(dp[i - 1], dp[i - 2] + person[i])
+    }
+    bw.write("${dp[n - 1]}")
+
+    bw.close()
+    br.close()
+}
