f4d54592412c164aa3f3bb1673af96fb5df4f3be
Solunax <97011241+Solunax@users.noreply.github.com>
2024-02-23 00:18:56 +0900

    [Bronze V] Title: Mathematics, Time: 84 ms, Memory: 12252 KB -BaekjoonHub

---
+++ 백준/Bronze/26545. Mathematics/Mathematics.kt
@@ -0,0 +1,18 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    var sum = 0
+
+    repeat(br.readLine().toInt()) {
+        sum += br.readLine().toInt()
+    }
+
+    bw.write("$sum")
+
+    bw.close()
+    br.close()
+}
