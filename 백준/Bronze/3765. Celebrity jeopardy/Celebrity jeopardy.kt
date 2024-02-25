2bd960d45e802678c940091712ecc4ce56176f12
Solunax <97011241+Solunax@users.noreply.github.com>
2024-02-26 00:11:11 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 88 ms, Memory: 12124 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.kt
@@ -0,0 +1,20 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    while (true) {
+        val input = br.readLine()
+
+        if (input.isNullOrEmpty()) {
+            break
+        } else {
+            bw.write("$input\n")
+        }
+    }
+
+    bw.close()
+    br.close()
+}
