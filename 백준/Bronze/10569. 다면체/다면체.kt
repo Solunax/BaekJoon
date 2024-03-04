2a4813cacaba3bdfb7901db582bae4cc142bb298
Solunax <97011241+Solunax@users.noreply.github.com>
2024-03-05 00:17:21 +0900

    [Bronze III] Title: 다면체, Time: 132 ms, Memory: 20648 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.kt
@@ -0,0 +1,15 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    repeat(br.readLine().toInt()) {
+        val (v, e) = br.readLine().split(" ").map { it.toInt() }
+        bw.write("${2 - (v - e)}\n")
+    }
+
+    bw.close()
+    br.close()
+}
