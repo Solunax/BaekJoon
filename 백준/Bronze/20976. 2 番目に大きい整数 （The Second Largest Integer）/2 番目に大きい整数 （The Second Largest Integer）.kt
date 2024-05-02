9c059083400e17db2aaa1a20d68c7a8777d1383c
Solunax <97011241+Solunax@users.noreply.github.com>
2024-05-03 00:27:53 +0900

    [Bronze IV] Title: 2 番目に大きい整数 (The Second Largest Integer), Time: 160 ms, Memory: 24196 KB -BaekjoonHub

---
+++ 백준/Bronze/20976. 2 番目に大きい整数 （The Second Largest Integer）/2 番目に大きい整数 （The Second Largest Integer）.kt
@@ -0,0 +1,13 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    val input = br.readLine().split(" ").map { it.toInt() }.sorted()
+    bw.write("${input[1]}")
+
+    bw.close()
+    br.close()
+}
