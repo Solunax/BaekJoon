9435866810a5c7c9cdc283f582d720cdcabaf0a7
Solunax <97011241+Solunax@users.noreply.github.com>
2024-05-17 00:13:12 +0900

    [Bronze V] Title: 제리와 톰, Time: 140 ms, Memory: 21356 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.kt
@@ -0,0 +1,13 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    val (a, b) = br.readLine().split(" ").map { it.toInt() }
+    bw.write("${b - a} $b")
+
+    bw.close()
+    br.close()
+}
