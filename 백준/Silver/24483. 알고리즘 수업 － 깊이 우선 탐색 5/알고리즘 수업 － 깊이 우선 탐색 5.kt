4d4fa881f29fb4c97bc9640104a4e01ed59b5189
Solunax <97011241+Solunax@users.noreply.github.com>
2024-08-01 00:29:14 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 5, Time: 1272 ms, Memory: 131792 KB -BaekjoonHub

---
+++ 백준/Silver/24483. 알고리즘 수업 － 깊이 우선 탐색 5/알고리즘 수업 － 깊이 우선 탐색 5.kt
@@ -0,0 +1,48 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+lateinit var map: Array<ArrayList<Int>>
+lateinit var visit: LongArray
+lateinit var sequence: LongArray
+var index = 1L
+
+fun main() {
+    val (n, m, r) = br.readLine().split(" ").map { it.toInt() }
+    map = Array(n + 1) { ArrayList() }
+    visit = LongArray(n + 1)
+    sequence = LongArray(n + 1) { -1 }
+
+    repeat(m) {
+        val (u, v) = br.readLine().split(" ").map { it.toInt() }
+        map[u].add(v)
+        map[v].add(u)
+    }
+
+    map.forEach {
+        it.sort()
+    }
+
+    search(r, 0)
+
+    var sum = 0L
+    for (i in 1..n) {
+        sum += visit[i] * sequence[i]
+    }
+    bw.write("$sum")
+
+    bw.close()
+    br.close()
+}
+
+fun search(now: Int, seq: Long) {
+    sequence[now] = seq
+    visit[now] = index++
+
+    for (v in map[now]) {
+        if (visit[v] == 0L) {
+            search(v, seq + 1)
+        }
+    }
+}
