2e71deb66566470253c021ce2e7cb38678291561
Solunax <97011241+Solunax@users.noreply.github.com>
2024-10-25 00:34:17 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 76 ms, Memory: 12328 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.kt
@@ -0,0 +1,13 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    val n = br.readLine().toInt()
+    bw.write("${n - n / 11}")
+
+    bw.close()
+    br.close()
+}
