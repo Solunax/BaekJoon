fecc46cffb7fcd145937a99d53d9251af1352c76
Solunax <97011241+Solunax@users.noreply.github.com>
2024-11-07 00:19:56 +0900

    [Silver IV] Title: GCD 합, Time: 236 ms, Memory: 22568 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.kt
+++ 백준/Silver/9613. GCD 합/GCD 합.kt
@@ -1,15 +1,21 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.util.StringTokenizer
 
 val br = BufferedReader(InputStreamReader(System.`in`))
 val bw = System.out.bufferedWriter()
 
 fun main() {
     repeat(br.readLine().toInt()) {
-        val input = br.readLine().split(" ").map { it.toInt() }
-        val n = input[0]
-        val arr = input.subList(1, n + 1).sorted()
+        val input = StringTokenizer(br.readLine())
+        val n = input.nextToken().toInt()
+        val arr = ArrayList<Int>()
         var sum = 0L
+
+        repeat(n) {
+            arr.add(input.nextToken().toInt())
+        }
+
 
         for (i in 0 until n - 1) {
             for (j in i + 1 until n) {
