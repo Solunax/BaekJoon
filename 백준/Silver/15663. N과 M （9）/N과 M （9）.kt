e025161c8e1e8cf35782fddc0dc4fa483e54d808
Solunax <97011241+Solunax@users.noreply.github.com>
2024-12-04 00:10:32 +0900

    [Silver II] Title: N과 M (9), Time: 364 ms, Memory: 77896 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.kt
@@ -0,0 +1,51 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+lateinit var arr: List<Int>
+lateinit var visit: BooleanArray
+lateinit var resultArr: IntArray
+lateinit var resultSet: HashSet<String>
+var n = 0
+var m = 0
+
+fun main() {
+    val input = br.readLine().split(" ").map { it.toInt() }
+    n = input[0]
+    m = input[1]
+    arr = br.readLine().split(" ").map { it.toInt() }.sorted()
+    visit = BooleanArray(n)
+    resultArr = IntArray(m)
+    resultSet = LinkedHashSet<String>()
+
+    search(0)
+    resultSet.forEach {
+        bw.write("$it\n")
+    }
+
+    bw.close()
+    br.close()
+}
+
+fun search(d: Int) {
+    if (d == m) {
+        val sb = StringBuilder()
+
+        for (v in resultArr) {
+            sb.append("$v ")
+        }
+
+        resultSet.add(sb.toString())
+        return
+    }
+
+    for (i in 0 until n) {
+        if (!visit[i]) {
+            visit[i] = true
+            resultArr[d] = arr[i]
+            search(d + 1)
+            visit[i] = false
+        }
+    }
+}
