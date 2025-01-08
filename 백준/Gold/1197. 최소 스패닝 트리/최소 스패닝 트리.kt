355f363e497afb3cee98a10332d83d1bdb485e52
Solunax <97011241+Solunax@users.noreply.github.com>
2025-01-09 00:51:03 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 712 ms, Memory: 82072 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.kt
@@ -0,0 +1,49 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.PriorityQueue
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    val (n, m) = br.readLine().split(" ").map { it.toInt() }
+    val nodes = Array<ArrayList<Node>>(n + 1) { ArrayList() }
+    val visit = BooleanArray(n + 1)
+    val pq = PriorityQueue<Node>()
+
+    repeat(m) {
+        val (v1, v2, weight) = br.readLine().split(" ").map { it.toInt() }
+
+        nodes[v1].add(Node(v2, weight))
+        nodes[v2].add(Node(v1, weight))
+    }
+
+    var sum = 0
+    pq.offer(Node(1, 0))
+    while (pq.isNotEmpty()) {
+        val now = pq.poll()
+        val t = now.target
+        val w = now.weight
+
+        if (!visit[t]) {
+            visit[t] = true
+            sum += w
+
+            for (node in nodes[t]) {
+                if (!visit[node.target]) {
+                    pq.offer(node)
+                }
+            }
+        }
+    }
+    bw.write("$sum")
+
+    bw.close()
+    br.close()
+}
+
+class Node(val target: Int, val weight: Int) : Comparable<Node> {
+    override fun compareTo(other: Node): Int {
+        return this.weight - other.weight
+    }
+}
