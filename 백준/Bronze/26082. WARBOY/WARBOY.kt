3caed5b2c134da737f1ba328c87d0d398a9356ec
Solunax <97011241+Solunax@users.noreply.github.com>
2025-03-24 00:07:18 +0900

    [Bronze V] Title: WARBOY, Time: 116 ms, Memory: 21376 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.kt
@@ -0,0 +1,13 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = System.out.bufferedWriter()
+
+fun main() {
+    val (a, b, c) = br.readLine().split(" ").map { it.toInt() }
+    bw.write("${b / a * 3 * c}")
+
+    bw.close()
+    br.close()
+}
